1a2894ec1154aca7497f665517a4adb7ff49816b
Gerardo <gerardofilho56@gmail.com>
2024-09-09 10:10:25 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'life-cycle';
+
+  valor: number = 5;
+  deletarCiclo: boolean = false;
+  mudarValor() {
+    this.valor++;
+  }
+  destruirClico() {
+    this.deletarCiclo = true;
+  }
+}
